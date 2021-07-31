aa8e23af70f68c386c568f47ba347fddd02aeb5f
setaylo <setaylo@users.noreply.github.com>
2021-07-31 03:24:10 +0000

    deploy: 1883ee6a8fce47382d749374b929c21b5c39a335

--- js/main.js
+++ js/main.js
@@ -17,3 +17,8 @@ function nightfall() {
   var element = document.getElementById("three");
   element.classList.add("active");
 }
+
+function chatwalk() {
+  var element = document.getElementById("chat-walk");
+  element.classList.add("chat-play");
+}
